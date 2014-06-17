ebfcafc0ee737a4aec7458a40caefe8cf90b8d03
Nicholas Dronen <nicholas.dronen@pearson.com>
2014-06-17 13:27:07 -0600

    Moved Predictor class from pylearnutils.py to __init__.py.

--- pylearnutils/__init__.py
+++ pylearnutils/__init__.py
@@ -3,3 +3,21 @@
 __author__ = 'Nicholas Dronen'
 __email__ = 'ndronen@gmail.com'
 __version__ = '0.1.0'
+
+class Predictor(object):
+    """
+    General comments about the class.
+    """
+    def __init__(self, model):
+        """
+        Constructor documentation.
+        model: what the model is.
+        """
+        self.model = model
+
+    def predict(X):
+        """
+        Predict function documentation.
+        X: what X is.
+        """
+        model.fprop(X)
--- pylearnutils/pylearnutils.py
+++ pylearnutils/pylearnutils.py
@@ -1,19 +1,1 @@
 # -*- coding: utf-8 -*-
-
-class Predictor(object):
-    """
-    General comments about the class.
-    """
-    def __init__(self, model):
-        """
-        Constructor documentation.
-        model: what the model is.
-        """
-        self.model = model
-
-    def predict(X):
-        """
-        Predict function documentation.
-        X: what X is.
-        """
-        model.fprop(X)
